4ad27b5bd512c4928dba0b745b1eea64f8c368bb
Aasawari <aasawari.sahasrabuddhe@mongodb.com>
2025-06-10 12:49:56 +0530

    Adding files for starter kit

--- config/database.php
+++ config/database.php
@@ -116,7 +116,7 @@ return [
         'driver' => 'mongodb',
         'dsn' => env('MONGODB_URI', 'mongodb://localhost:27017'),
         'database' => env('MONGODB_DATABASE', 'laravel_app'),
-    ],
+        ],
 
     ],
 
